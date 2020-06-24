24b120017857eebcff65e8dd6886f0f6a3ec4276
maluarmini <luiza.armini@gmail.com>
2020-06-24 18:46:47 -0300

    Frontend-web da aplicação

---
+++ web/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+// renderizando a App dentro da div com nome 'root', la no index.html
